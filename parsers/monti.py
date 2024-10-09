7ce7758202ad6b72e529e6d2c49d52b16f4b080d
mkey <fads@gmail.com>
2024-10-09 16:03:27 +0800

    monti

--- parsers/monti.py
+++ parsers/monti.py
@@ -8,30 +8,45 @@
 Rappel : def appender(post_title, group_name, description="", website="", published="", post_url="")
 """
 
-import os,datetime
-from bs4 import BeautifulSoup
+from lxml import etree
 
-def main(scrapy,page,site):
+
+# 单独爬取一个post
+def get_post(scrapy, site, url, published):
+    try:
+        page = scrapy.scrape(site, url)
+
+        if page is None:
+            return None
+
+        # todo 提取相关字段
+        html = etree.HTML(page["page_source"])
+        post_title = html.xpath("//h2/text()")[0]
+
+        contents = ""
+        bodys = html.xpath("//text()")
+        for body in bodys:
+            contents += body
+
+        download = html.xpath("//a[@target='_blank']/@href")
+
+        scrapy.appender(post_title, "monti", contents, "", post_url=url,
+                        published=published, download=download, page=page)
+    except Exception as e:
+        print(f'monti: parsing fail: {url} : {e}')
+
+
+def main(scrapy, page, site):
     url = page["domain"]
     try:
-        soup=BeautifulSoup(page["page_source"],'html.parser')
-        # divs_name=soup.find_all('a', {"class": "leak-card p-3"})
-        divs_name = soup.find_all('div', {"class": "col-lg-4 col-sm-6 mb-4"})
-        for div in divs_name:
-            title = div.find('h5').text.strip()
-            post = div.find('a')
-            post = post.get('href')
-            #parts = filename.split('-')
-            #url = 'mblogci3rudehaagbryjznltdp33ojwzkq6hn2pckvjq33rycmzczpid'
-            #try:
-            post_url =  url + post
-            #except:
-            #    url = ''
-            description =  div.find('p').text.strip()
-            published = div.find('div', {'class': 'col-auto published'}).text.strip()
-            date_obj =  datetime.datetime.strptime(published, '%Y-%m-%d %H:%M:%S')
-            published = date_obj.strftime("%Y-%m-%d %H:%M:%S.%f")
-            scrapy.appender(title, 'monti', description,"",published,post_url,page=page)
-        
-    except:
-        print('monti: ' + 'parsing fail: '+url)
+        html = etree.HTML(page["page_source"])
+        hrefs = html.xpath("//a[@class='leak-card p-3']/@href")
+        publisheds = html.xpath("//a[@class='leak-card p-3']//div[@class='col-auto published']")
+        for href in range(0, len(hrefs)):
+            post_url = "http://" + url + hrefs[href]
+            published = publisheds[href]
+
+            get_post(scrapy, site, post_url, published)
+
+    except Exception as e:
+        print(f'monti: parsing fail: {url} : {e}')
